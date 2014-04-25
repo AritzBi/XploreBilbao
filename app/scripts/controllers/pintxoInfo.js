4a81a0bdc33b7e8ecfe4648693376e3ae9078115
AritzBi <aritzbi@gmail.com>
2014-04-25 23:57:03 +0200

    Map embeded started

--- app/scripts/controllers/pintxoInfo.js
+++ app/scripts/controllers/pintxoInfo.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('xploreBilbaoApp')
-	.controller('PintxoInfoCtrl', function ($scope,$stateParams, Pintxo){
+	.controller('PintxoInfoCtrl', function ($scope,$stateParams, $sce, Pintxo){
 		$scope.pintxo=Pintxo.get({id: $stateParams.id});
+		$scope.url = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCAu20ELj_7PB4PeBG1rlBLJOnHsWJ1z_w&q=" + $scope.pintxo.address;
+		$scope.url = $sce.trustAsResourceUrl($scope.url);
 	});
--- app/scripts/controllers/restaurantsInfo.js
+++ app/scripts/controllers/restaurantsInfo.js
@@ -3,4 +3,5 @@
 angular.module('xploreBilbaoApp')
 	.controller('RestaurantsInfoCtrl', function ($scope,$stateParams, Restaurant){
 		$scope.restaurant=Restaurant.get({id: $stateParams.id});
+		$scope.url = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCAu20ELj_7PB4PeBG1rlBLJOnHsWJ1z_w&q=" + $scope.restaurant.address;
 	});
